47d8c418c75444151b7a71944a1250228d53c36f
Oleh Krotovych <OlgastDev@gmail.com>
2021-02-21 23:17:28 +0100

    Fix parent navigation

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -15,7 +15,9 @@ export default () => {
       },
     });
 
-    history.listen(onParentNavigate);
+    if (onParentNavigate) {
+      history.listen(onParentNavigate);
+    }
   }, []);
 
   return <div ref={ref} />;
